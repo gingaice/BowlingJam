5b597aa950c8d5685a59f6aab461dba0ca299fc9
MikeHolce <mh05252443@priestley.ac.uk>
2021-06-17 10:30:46 +0100

    added power

--- Assets/Code/BigFishWeBalin.cs
+++ Assets/Code/BigFishWeBalin.cs
@@ -7,16 +7,26 @@ public class BigFishWeBalin : MonoBehaviour
 
     public Rigidbody m_rigidbody;
 
+    public float shootPower;
+    public float Multiplier;
+
     public int num;
 
     public bool freezeAngles;
+    public bool startPower;
+
+    public bool up;
+    public bool down;
 
     // Start is called before the first frame update
     void Start()
     {
         freezeAngles = false;
+        startPower = false;
 
         m_rigidbody = GetComponent<Rigidbody>();
+
+        Multiplier = 31;
     }
 
     // Update is called once per frame
@@ -25,11 +35,42 @@ public class BigFishWeBalin : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             freezeAngles = true;
+            startPower = true;
         }
 
         if(freezeAngles == true)
         {
-
+            if (Input.GetKeyDown(KeyCode.UpArrow))// when pressed lauches ball
+            {
+                startPower = false;
+                StopCoroutine(Timer());
+                m_rigidbody.velocity = shootPower * transform.forward;
+            }
         }
+
+        if(startPower == true)
+        {
+            StartCoroutine(Timer());
+        }
+
+        if(up == true)
+        {
+            shootPower += Multiplier;
+        }
+
+        if (down == true)
+        {
+            shootPower -= Multiplier;
+        }
+
+    }
+
+    IEnumerator Timer()
+    {
+        up = true;
+        yield return new WaitForSeconds(1);
+        down = true;
+        yield return new WaitForSeconds(1);
+        StartCoroutine(Timer());
     }
 }
